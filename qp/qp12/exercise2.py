4dfe505300aa023c35301596659dd61f0597f4e0
Simão Barbosa <117586175+simaopsbarbosa@users.noreply.github.com>
2023-12-13 01:31:58 +0000

    Update exercise2.py

--- qp/qp12/exercise2.py
+++ qp/qp12/exercise2.py
@@ -1,23 +1,26 @@
-def string_to_matrix(board):
-    '''
-    aux function to transform a string board
-    into a list of lists for better visibilty
-    '''
-    # ' xx\n o \noxo'
-    return [[board[0], board[1], board[2]],
-            [board[4], board[5], board[6]],
-            [board[8], board[9], board[10]]]
-
-
 def tic_tac_toe(board: str, player):
-
-    # winning move only
-
-    # check sides and diagonals
-
-    board.split("\n")
-    "".join(board)
-    return board
-
-
-print(tic_tac_toe(" xx\n o \noxo", 1))
+    correspondencia = {0:"A", 1:"B", 2:"C"}
+    
+    # check linhas
+    for i in range(3):
+        linha = [board[4 * i], board[4 * i + 1], board[4 * i + 2]]
+        if linha.count(player) == 2 and linha.count(' ') == 1:
+            return f"{correspondencia.get(i)}{linha.index(' ')+1}"
+    
+    # check colunas
+    for i in range(3):
+        coluna = [board[i], board[4 + i], board[8 + i]]
+        if coluna.count(player) == 2 and coluna.count(' ') == 1:
+            return f"{correspondencia.get(coluna.index(' '))}{i+1}"
+        
+    # check diagonais
+    diag1 = [board[8], board[5], board[2]]
+    if diag1.count(player) == 2 and diag1.count(' ') == 1:
+        corr2 = {0:"C1", 1:"B2", 2:"A3"}
+        return f"{corr2.get(diag1.index(' '))}"
+    
+    diag2 = [board[0], board[5], board[10]]
+    if diag2.count(player) == 2 and diag2.count(' ') == 1:
+        corr2 = {0:"A1", 1:"B2", 2:"C3"}
+        return f"{corr2.get(diag2.index(' '))}"
+    
